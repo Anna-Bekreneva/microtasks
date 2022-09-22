8ac7f68fb0653315045627bf1b0dc1d26aca535e
Anna-Bekreneva <anjabekreneva@gmail.com>
2022-09-22 19:14:12 +0300

    lesson 5 filter

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,53 @@ import React, {useState} from 'react';
 import './App.css';
 import {Button} from './components/Button';
 
-type FilterType = 'all' | 'dollar' | 'ruble'
-
 function App() {
+	type FilterType = 'all' | 'dollar' | 'ruble'
+	const [money, setMoney] = useState([
+		{ banknots: 'Dollars', value: 100, number: ' a1234567890' },
+		{ banknots: 'Dollars', value: 50, number: ' z1234567890' },
+		{ banknots: 'RUBLS', value: 100, number: ' w1234567890' },
+		{ banknots: 'Dollars', value: 100, number: ' e1234567890' },
+		{ banknots: 'Dollars', value: 50, number: ' c1234567890' },
+		{ banknots: 'RUBLS', value: 100, number: ' r1234567890' },
+		{ banknots: 'Dollars', value: 50, number: ' x1234567890' },
+		{ banknots: 'RUBLS', value: 50, number: ' v1234567890' },
+	])
 
-	// let a = 1
+	const [filter, setFilter] = useState<FilterType>('all')
 
-	let [a, setA] = useState(1)
-
-	const number = () => {
-		setA(++a)
+	const onClickFilterHundler = (nameButton: FilterType) => {
+		setFilter(nameButton)
 	}
 
-	const reset = () => {
-		setA(0)
+	let currentMoney = money;
+	if (filter === "ruble") {
+		currentMoney = money.filter((el) => el.banknots === "RUBLS")
+	}
+	if (filter === "dollar") {
+		currentMoney = money.filter((el) => el.banknots === "Dollars")
 	}
 
 	return (
 		<>
-			<h1>{a}</h1>
-			<button onClick={number}>number</button>
-			<button onClick={reset}>reset</button>
+			<ul>
+				{currentMoney.map((objFromMontyArr, index) => {
+					return (
+						<li key={index}>
+							<span></span>
+							<span>{objFromMontyArr.banknots}</span>
+							<span>{objFromMontyArr.value}</span>
+							<span>{objFromMontyArr.number}</span>
+						</li>
+					);
+				})}
+			</ul>
+
+			<button onClick={() => (onClickFilterHundler("all"))}>all</button>
+			<button onClick={() => (onClickFilterHundler('ruble'))}>ruble</button>
+			<button onClick={() => (onClickFilterHundler('dollar'))}>dollar</button>
 		</>
-	)
+	);
 }
 
 export default App;
